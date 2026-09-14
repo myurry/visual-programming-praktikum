cb916bb7c3549e7f26fed06598397b675658f139
Semjon Suhhotin <myurry255@gmail.com>
2026-09-14 17:20:45 +0300

    KiviPaber App

---
+++ KiviPaber.WpfApp/KiviPaber.Core/Enums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KiviPaber.Core
+{
+    public enum Move
+    {
+        Rock = 0,
+        Paper = 1,
+        Scissors = 2    
+    }
+
+    public enum RoundResult
+    {
+        Win,
+        Lose,
+        Draw
+    }  
+
+    public struct GameRound
+    {
+        public int Number { get; set; }
+        public Move PlayerMove { get; set; }
+        public Move ComputerMove { get; set; }
+        public RoundResult Result { get; set; }
+    }
+
+
+}
---
+++ KiviPaber.WpfApp/KiviPaber.Core/GameLogic.cs
@@ -0,0 +1,33 @@
+﻿namespace KiviPaber.Core
+{
+    public static class GameLogic
+    {
+        public static Move GetComputerMove()
+        {
+            // Simple AI logic - randomly select a move
+            Random random = new Random();
+            int moveIndex = random.Next(0, 3);
+            return (Move)moveIndex;
+        }
+
+        public static RoundResult GetResult(Move playerMove, Move computerMove)
+        {
+            if (playerMove == computerMove)
+            {
+                return RoundResult.Draw;
+            }
+
+            switch (playerMove)
+            {
+                case Move.Rock:
+                    return computerMove == Move.Scissors ? RoundResult.Win : RoundResult.Lose;
+                case Move.Paper:
+                    return computerMove == Move.Rock ? RoundResult.Win : RoundResult.Lose;
+                case Move.Scissors:
+                    return computerMove == Move.Paper ? RoundResult.Win : RoundResult.Lose;
+                default:
+                    throw new ArgumentException("Invalid move");
+            }
+        }
+    }
+}
